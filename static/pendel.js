386734392a39c62bffbdcd39af89543c1cac34e2
janvonrickenbach <janvonrickenbach@gmail.com>
2021-04-15 23:26:32 +0200

    Variable fontsize

--- static/pendel.js
+++ static/pendel.js
@@ -1,21 +1,6 @@
- 
-//http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=red&dt=13
-//http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&dt=20000
-//http://127.0.0.1:5500/?dt=1000&a=1&omega=5&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=1000&a=1&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=1000&a=1&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=2000&a=1&omega=100&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=2000&a=100&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=12345480&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=1100001&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=1100000&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=70000000&a=7&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=1290000000&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-//http://127.0.0.1:5500/?dt=129&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-
-var dt 
-var time
-var amp
+var dt;
+var time;
+var amp;
 
 function create_line(){
     return {
@@ -31,37 +16,36 @@ function create_line(){
         start: {x: 0, y:0},
         end: {x: 0, y:0}
     }
-}
+};
 
-var line1
-var line2
+var line1;
+var line2;
 
-var fontsize = 100
-var messages 
+var messages; 
 
-let font;
+let font = {
+    size: 1000,
+    type: undefined
+};
+
 function preload() {
-  font = loadFont('static/inconsolata.ttf');
+  font.type = loadFont('static/inconsolata.ttf');
 }  
 
 function setup() {
-    line1= create_line()
-    line2= create_line()
+    line1= create_line();
+    line2= create_line();
     var params = getURLParams();
-    line1.speed.a = params.a
-    line2.speed.a = -params.a
+    line1.speed.a = params.a;
+    line2.speed.a = -params.a;
 
-    line1.speed.omega = params.omega
-    line2.speed.omega = params.omega
-    var max_speed = max(params.a,params.omega)
-    if(params.dt < 0){
-        dt = 1/(params.dt* max_speed)
-    } else {
-        dt = params.dt/(max_speed)
-    }
+    line1.speed.omega = params.omega;
+    line2.speed.omega = params.omega;
 
-    line1.color = params.color1
-    line2.color = params.color2
+    dt =get_dt(params);
+
+    line1.color = params.color1;
+    line2.color = params.color2;
 
     var canvas_size = min(windowWidth, windowHeight);
     createCanvas(canvas_size, canvas_size).parent('canvasHolder');
@@ -78,12 +62,20 @@ function setup() {
     var margin = 0.25*canvas_size
     
     line_height = (canvas_size- 2*margin)/ lines 
-    messages = texts
+    var set_messages  = () => {
+        messages = texts
         .map((text, idx) => create_message(text, idx, margin,canvas_size))
- 
-    textFont(font)
-    textSize(fontsize)
+    }
 
+    set_messages();
+
+    while(messages.some(m => is_message_too_big(m, canvas_size, margin, line_height))){
+        font.size -= font.size * 0.1
+        set_messages();
+    }
+ 
+    textFont(font.type)
+    textSize(font.size)
     strokeWeight(0.4);
 }
 
@@ -104,7 +96,7 @@ function draw() {
 }
 
 function create_message(text, line_index,margin, canvas_size){
-    var bbox = font.textBounds(text, 200, 200, fontsize)
+    var bbox = font.type.textBounds(text, 200, 200, font.size)
     var y= margin+ (line_index+1)*line_height
     return {
         text:text,
@@ -145,3 +137,34 @@ function update_state(line){
     line.state.alpha += line.speed.a*dt
     line.state.beta +=line.speed.omega*dt
 }
+
+function is_message_too_big(message, canvas_size, margin, line_height){
+    return message.w > (canvas_size - margin) || message.h > line_height
+}
+
+function get_dt(params){
+    var max_speed = max(params.a,params.omega)
+    if(params.dt < 0){
+        return 1/(params.dt* max_speed)
+    } else {
+        return params.dt/(max_speed)
+    }
+
+}
+
+
+// Good combinations
+//http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=red&dt=13
+//http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=red&dt=13
+//http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&dt=20000
+//http://127.0.0.1:5500/?dt=1000&a=1&omega=5&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=1000&a=1&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=1000&a=1&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=2000&a=1&omega=100&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=2000&a=100&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=12345480&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=1100001&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=1100000&a=2&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=70000000&a=7&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=1290000000&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
+//http://127.0.0.1:5500/?dt=129&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
