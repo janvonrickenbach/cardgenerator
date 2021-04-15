402e461e974ea67c62b86fabfc3b31d6c60d126e
janvonrickenbach <janvonrickenbach@gmail.com>
2021-04-15 19:44:51 +0200

    cleanup

--- static/pendel.js
+++ static/pendel.js
@@ -1,5 +1,4 @@
-var time = 0;
-var dt  
+ 
 //http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=red&dt=13
 //http://127.0.0.1:5500/?a=100&omega=10&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&dt=20000
 //http://127.0.0.1:5500/?dt=1000&a=1&omega=5&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
@@ -13,103 +12,136 @@ var dt
 //http://127.0.0.1:5500/?dt=70000000&a=7&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
 //http://127.0.0.1:5500/?dt=1290000000&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
 //http://127.0.0.1:5500/?dt=129&a=8&omega=1&text1=Happy&text2=New&text3=Year&color1=blue&color2=orange&
-var x1
-var x2
-var y1
-var y2
 
-var prev_x1
-var prev_x2
-var prev_y2
-var prev_y1
-var canvas_size = 500
-
-var a
-var omega
-var beta
-
+var dt 
+var time
 var amp
-var text_w = []
-var text_h = []
-var text_x
-var text_y
+
+function create_line(){
+    return {
+        state: {
+            alpha:0,
+            beta:0
+        },
+        speed: {
+            a:0,
+            omega:0
+        },
+        color: undefined,
+        start: {x: 0, y:0},
+        end: {x: 0, y:0}
+    }
+}
+
+var line1
+var line2
+
 var fontsize = 100
 var messages 
 
-var dt_fac
-
-var points
-var params
 let font;
 function preload() {
-  
   font = loadFont('static/inconsolata.ttf');
 }  
 
 function setup() {
-    params = getURLParams();
-    a = params.a
-    omega = params.omega
+    line1= create_line()
+    line2= create_line()
+    var params = getURLParams();
+    line1.speed.a = params.a
+    line2.speed.a = -params.a
+
+    line1.speed.omega = params.omega
+    line2.speed.omega = params.omega
+    var max_speed = max(params.a,params.omega)
     if(params.dt < 0){
-        dt = 1/(params.dt* max(a,omega))
+        dt = 1/(params.dt* max_speed)
     } else {
-        dt = params.dt/(max(a,omega))
+        dt = params.dt/(max_speed)
     }
-    messages = [params.text1, params.text2, params.text3]
-    beta = 0
-    canvas_size = min(windowWidth, windowHeight);
+
+    line1.color = params.color1
+    line2.color = params.color2
+
+    var canvas_size = min(windowWidth, windowHeight);
     createCanvas(canvas_size, canvas_size).parent('canvasHolder');
     amp = canvas_size /2 
-    prev_x1 = get_x(0, amp,a,beta)
-    prev_y1 = get_y(0, amp,a,beta)
-    x1 = get_x(0,amp,a,beta)
-    y1 = get_y(0,amp,a,beta)
 
-    prev_x2 = get_x(0, amp,a,beta)
-    prev_y2 = get_y(0, amp,a,beta)
-    x2 = get_x(0,amp,a,beta)
-    y2 = get_y(0,amp,a,beta)
+    line1.start= get_point(amp, line1.state)
+    line1.end = get_point( amp, line1.state)
+
+    line2.start= get_point(amp, line2.state)
+    line2.end = get_point( amp, line2.state)
+
+    var texts = [params.text1, params.text2, params.text3]
+    var lines = texts.length
+    var margin = 0.25*canvas_size
     
-    let bboxes = messages.map(message => font.textBounds(message, 10, 10, fontsize));
-    text_w = bboxes.map(bbox => bbox.w)
-    text_h = bboxes.map(bbox => bbox.h)
-    var lines = 3
-    line_height = canvas_size / lines
-    line_heights = [0.3, 1,1.7]
-    text_y = text_h.map((height, idx) => line_heights[idx]*line_height + height + (line_height-height) /2 )
-    text_x = text_w.map(width => (canvas_size - width)/2)
+    line_height = (canvas_size- 2*margin)/ lines 
+    messages = texts
+        .map((text, idx) => create_message(text, idx, margin,canvas_size))
+ 
     textFont(font)
     textSize(fontsize)
-    strokeWeight(0.4);
-    fill(255);
-}
 
+    strokeWeight(0.4);
+}
 
 function draw() {
     time += dt;
-    beta +=omega*dt
-    prev_x1 = x1
-    prev_y1 = y1
-    x1 = get_x(time, amp,a,beta)
-    y1 = get_y(time, amp,a,beta)
-    prev_x2 = x2
-    prev_y2 = y2
-    x2 = get_x(-time, amp,a,beta)
-    y2 = get_y(-time, amp,a,beta)
-    stroke(params.color1);
+    update_line(line1, amp)
+    update_line(line2, amp)
 
-    line(prev_x1,prev_y1, x1,y1)
-    stroke(params.color2);
-    line(prev_x2,prev_y2, x2,y2)
-    stroke(255)
+    draw_line(line1)
+    draw_line(line2)
 
-    messages.map((message,idx) => text(message, text_x[idx], text_y[idx]));    
+    messages.map(message => {
+        fill(255)
+        stroke(255)
+        textAlign(CENTER, BOTTOM);
+        text(message.text,message.x, message.y)
+    });    
 }
 
-function get_x(loc_t,amp,a,beta){
-    return sin(a*loc_t) * cos(beta) * 0.9*amp + amp
+function create_message(text, line_index,margin, canvas_size){
+    var bbox = font.textBounds(text, 200, 200, fontsize)
+    var y= margin+ (line_index+1)*line_height
+    return {
+        text:text,
+        w: bbox.w,
+        h: bbox.h,
+        y:y,
+        x:canvas_size/2
+    }
 }
 
-function get_y(loc_t,amp,a,beta){
-    return  sin(a*loc_t) * sin(beta) * 0.9*amp + amp
+function get_point( amp, state){
+    return {
+        x: get_x(amp, state),
+        y: get_y(amp, state)
+    }
+}
+
+function get_x(amp, state){
+    return sin(state.alpha) * cos(state.beta) * 0.9*amp + amp
+}
+
+function get_y(amp, state){
+    return  sin(state.alpha) * sin(state.beta) * 0.9*amp + amp
+}
+
+function update_line(line, amp){
+    update_state(line)
+    line.start = line.end
+    line.end = get_point(amp, line.state)
+}
+
+function draw_line(line_input){
+    stroke(line_input.color);
+    line(line_input.start.x, line_input.start.y, line_input.end.x,line_input.end.y)
+}
+
+function update_state(line){
+    line.state.alpha += line.speed.a*dt
+    line.state.beta +=line.speed.omega*dt
 }
